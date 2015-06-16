6c27dfdaf578fa8757e53a2ed70f7a3b2c06ddea
Andrea <andrea.petreri@gmail.com>
2015-06-16 18:03:43 +0200

    Travis CI integration

--- app/src/androidTest/java/com/thetonrifles/activitydetector/ApplicationTest.java
+++ app/src/androidTest/java/com/thetonrifles/activitydetector/ApplicationTest.java
@@ -3,11 +3,20 @@ package com.thetonrifles.activitydetector;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import junit.framework.Assert;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
     public ApplicationTest() {
         super(Application.class);
     }
+
+    public void testNumberUtils() throws Exception {
+        // stupid test
+        Assert.assertTrue(true);
+    }
+
 }
